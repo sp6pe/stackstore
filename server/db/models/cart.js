121eaab09f1493146d13e4c6376dadec2794dcd0
Franklin Lee <BigBoss@Franklins-MacBook-Pro.local>
2016-03-01 12:35:56 -0500

    made tests, and made improvements to user routes and cart model '

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -6,7 +6,15 @@ var schema = new mongoose.Schema({
     productList: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Product"
-    }]
+    }],
+    creation:{
+    	type:Date, 
+    	default: Date.now
+    },
+    status:{
+    	type:String,
+    	enum:['created', 'processing','cancelled','complete']
+    }
 });
 
 mongoose.model('Cart', schema);
