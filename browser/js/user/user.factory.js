b49d05cf786deab04bbf8a348f905a04e3545d98
Franklin Lee <BigBoss@Franklins-MacBook-Pro.local>
2016-03-02 11:51:22 -0500

    created user factory

---
+++ browser/js/user/user.factory.js
@@ -0,0 +1,47 @@
+'use strict';
+
+app.factory("UserFactory", function($http) {
+	var UserFactory = {};
+
+	UserFactory.fetchAll = function() {
+		return $http.get('/api/users')
+		.then(function(response) {
+			console.log("UserFactory.fetchAll: ", response.data);
+			return response.data;
+		})
+	}
+
+	UserFactory.fetchById = function(id) {
+		return $http.get('/api/users/' + id) 
+		.then(function(response) {
+			console.log("UserFactory.fetchById: ", response.data);
+			return response.data;
+		})
+	}
+
+	UserFactory.create = function() {
+		return $http.post('/api/users')
+		.then(function(response) {
+			console.log("UserFactory.create: ", response.data);
+			return response.data;
+		})
+	}
+
+	UserFactory.update = function(id) {
+		return $http.put('/api/users/' + id)
+		.then(function(response) {
+			console.log('UserFactory.update: ', response.data);
+			return response.data;
+		})
+	}
+
+	UserFactory.remove = function(id) {
+		return $http.delete('/api/users/' + id)
+		.then(function(response) {
+			console.log('UserFactory.remove: ', response.data);
+			return response.data;
+		})
+	}
+
+	return UserFactory;
+});
