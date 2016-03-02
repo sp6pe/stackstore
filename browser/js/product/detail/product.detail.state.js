4886bcf0ca06c8d920c41f37802bd5aa30c3edc1
Gustavo Ocasio <gocasiot@gmail.com>
2016-03-02 14:55:51 -0500

    Created product detail html, controller, and state

--- browser/js/product/detail/product.detail.state.js
+++ browser/js/product/detail/product.detail.state.js
@@ -1,7 +1,16 @@
 'use strict'
 
 app.config(function($stateProvider){
-	url:,
-	templateUrl:,
-	controller: ,
+
+	$stateProvider.state('product',{
+		url: '/products/:productId',
+		templateUrl: '/js/product/detail',
+		controller: 'ProductCtrl',
+		resolve: {
+			theProduct: function(ProductFactory, $stateParams){
+				return ProductFactory.fetchById($stateParams.productId);
+			}
+		}
+	});
+	
 });
