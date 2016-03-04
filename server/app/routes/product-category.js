16b8e82c637cd4bd338973bd97165bc0db5df71f
Sean Colandrea <Sean@Seans-MacBook-Pro-2.local>
2016-03-04 16:07:21 -0500

    Added some tabs...

--- server/app/routes/product-category.js
+++ server/app/routes/product-category.js
@@ -6,18 +6,18 @@ var Category = mongoose.model('Category');
 // For adding a category to a specific product
 router.post('/', function(req, res, next) {
 	req.product.addCategory(req.body)
-	.then(function(category) {
-		res.status(201).json(category);
-	})
-	.then(null, next);
+		.then(function(category) {
+			res.status(201).json(category);
+		})
+		.then(null, next);
 });
 
 router.delete('/:id',function(req,res,next){
 	Category.findById(req.params.id)
-	.then(function(cat){
-		return req.product.removeCategory(cat);
-	}).then(function(product){
-		res.send(product);
-	})
-	.then(null,next)
+		.then(function(cat){
+			return req.product.removeCategory(cat);
+		}).then(function(product){
+			res.send(product);
+		})
+		.then(null,next);
 });
--- server/app/routes/products.js
+++ server/app/routes/products.js
@@ -5,49 +5,49 @@ var Product = mongoose.model('Product');
 
 router.param('productId', function(req, res, next, productId) {
 	Product.findById(productId)
-	.populate('user categories')
-	.then(function(product) {
-		if (!product) throw new Error('Product not found');
-		req.product = product;
-		next();
-	})
-	.then(null, next);
+		.populate('user categories')
+		.then(function(product) {
+			if (!product) throw new Error('Product not found');
+			req.product = product;
+			next();
+		})
+		.then(null, next);
 });
 
 router.get('/', function(req, res, next) {
 	Product.find({})
-	.then(function(products) {
-		res.json(products);
-	})
-	.then(null, next);
+		.then(function(products) {
+			res.json(products);
+		})
+		.then(null, next);
 });
 
 router.get('/:productId', function(req, res, next) {
-	//console.log(req.product);
 	res.json(req.product);
 });
 
 router.post('/', function(req, res, next) {
 	Product.create(req.body)
-	.then(function(product) {
-		res.status(201).json(product);
-	})
-	.then(null, next);
+		.then(function(product) {
+			res.status(201).json(product);
+		})
+		.then(null, next);
 });
 
 router.put('/:productId', function(req, res, next) {
 	Product.findByIdAndUpdate(req.product._id, req.body, {new: true,runValidators: true})
-	.then(function(product) {
-		res.json(product);
-	})
-	.then(null, next);
+		.then(function(product) {
+			res.json(product);
+		})
+		.then(null, next);
 });
 
 router.delete('/:productId', function(req, res, next) {
 	req.product.remove()
-	.then(function(success) {
-		res.status(204).end();
-	})
+		.then(function(success) {
+			res.status(204).end();
+		})
+		.then(null, next);
 });
 
 router.use('/:productId/category', require('./product-category'));
